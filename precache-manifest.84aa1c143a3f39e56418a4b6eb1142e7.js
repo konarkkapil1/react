75bba38a7fe29a64f0b449f0d7f5e4f838ef246b
konarkkapil1 <konarkkapil@rocketmail.com>
2020-01-04 00:01:47 +0530

    Add files via upload

---
+++ precache-manifest.84aa1c143a3f39e56418a4b6eb1142e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09d6c14d76afee26fbbb6d6b9d1f82f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d98e5e69bb23358ebe28",
+    "url": "/static/css/main.f6b2db65.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d98e5e69bb23358ebe28",
+    "url": "/static/js/main.c3aa567f.chunk.js"
+  },
+  {
+    "revision": "ae7e7f13d555eece59cf",
+    "url": "/static/js/runtime-main.6b8bb62d.js"
+  }
+]);
